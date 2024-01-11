9990670cf2747a1728fa348d195fd32067da5f3f
solarix95 <rheld@gmx.net>
2024-01-11 23:42:14 +0100

    Page 50

---
+++ 13-arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿
+var values = new double[5];
+
+for (int i=0; i<values.Length; i++)
+    values[i] = i;
+
+for (int i=0; i<values.Length; i++)
+    Console.WriteLine(values[i]);
+
+values = new double[] {5,6,7,6,8,9};
+foreach(var value in values)
+    Console.WriteLine(value);
+
+Console.WriteLine("--------------------------------");
+double[] values2 = {10,11,12,13,14,15};
+foreach(var value in values2)
+    Console.WriteLine(value);
+
+Console.WriteLine("--------------------------------");
+double[] valueReference = values;
+valueReference[0] = 99;
+Console.WriteLine(valueReference[0]);
+Console.WriteLine(values[0]);
+
+valueReference = (double[])values.Clone(); // now not pointing to the same object.. same as "memcpy()" in C
+valueReference[0] = 100;
+Console.WriteLine(valueReference[0]);
+Console.WriteLine(values[0]);
+
+// Indices
+Console.WriteLine("--------------------------------");
+values[^1] = 999;
+values[^2] = 998;
+foreach(var value in values)
+    Console.WriteLine(value);
+
+Console.WriteLine("--------------------------------");
+Index last = ^1;
+values[last] = 1000;
+foreach(var value in values)
+    Console.WriteLine(value);
+
+Console.WriteLine("--------------------------------");
+double[] head = values[..3];
+for (int i=0; i<head.Length; i++)
+    head[i] = i;
+
+// "head" is a copy.. not a reference to a sub-Array:
+foreach(var value in head)
+    Console.WriteLine(value);
+foreach(var value in values)
+    Console.WriteLine(value);
+
+Console.WriteLine("--------------------------------");
+Range lastTwoValues = ^2..^0;
+double[] tail = values[lastTwoValues];
+foreach(var value in tail)
+    Console.WriteLine(value);
+
+    
---
+++ 14-parameters/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+static void callByValue(int v) { // C++ callByValue(int v)
+    v = 23;
+}
+
+static void callByRef(ref int v) { // C++ callByRef(int &v)
+    v = 23;
+}
+
+static void callByOut(out int v) { // C++ callByOut(int *v) (optional Reference.. in C++ a pointer with default "nullptr")
+    v = 23;
+}
+
+static void callByIn(in int v) { // C++ callByIn(const int &v)
+    // v = 23; Compile Error -> v is "const"
+}
+
+static void callByParams(out string s, params string[] parts) {
+    s = "";
+    for (int i=0; i<parts.Length; i++) {
+        s += parts[i] + " ";
+    }
+    s = s.Trim();
+}
+
+int x = 1;
+Console.WriteLine($"Before callByValue: {x}");
+callByValue(x);
+Console.WriteLine($"After callByValue: {x}");
+
+Console.WriteLine($"Before callByRef: {x}");
+callByRef(ref x);
+Console.WriteLine($"After callByRef: {x} (changed)");
+
+x = 1;
+Console.WriteLine($"Before callByOut: {x}");
+callByOut(out x);
+Console.WriteLine($"After callByOut: {x}");
+
+callByOut(out int x2);
+Console.WriteLine($"callByOut created new variable x2: {x}");
+
+Console.WriteLine($"callByOut without parameter");
+callByOut(out _);
+
+x = 1;
+Console.WriteLine($"Before callByIn: {x}");
+callByIn(in x);
+Console.WriteLine($"After callByIn: {x}");
+
+callByParams(out string result, "one", "two", "three");
+Console.WriteLine($"Params: {result}");
