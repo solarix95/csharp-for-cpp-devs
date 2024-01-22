b0cd9c8bf01df8041de6566b3a7e8226c5a40d2f
solarix95 <rheld@gmx.net>
2024-01-22 22:29:00 +0100

    Cleanup + new Example #18

--- 17-switch_expression/Program.cs
+++ 17-switch_expression/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+// switch expression
 using System.Security.Cryptography;
 
 string ToString(int i) {
@@ -17,3 +18,26 @@ Console.WriteLine(ToString(23));
 Console.WriteLine(ToString(42));
 Console.WriteLine(ToString(187));
 Console.WriteLine(ToString(666));
+
+
+// multidimensional switch expression
+string WeatherCondition(int temperature, string condition) {
+    return (temperature, condition) switch {
+        (>= 30, "Sunny") => "It's a hot and sunny day!",
+            (>= 30, "Cloudy") => "It's warm, but the sky is overcast.",
+            (>= 30, "Rainy") => "Hot and rainy weather. Keep dry!",
+            (< 30, "Sunny") => "It's a pleasant day with clear skies.",
+            (< 30, "Cloudy") => "The sky is overcast, and it might rain.",
+            (< 5, "Rainy") => "OMG don't leave your bed today.",
+            (< 30, "Rainy") => "Expect rain today. Don't forget your umbrella.",
+            (_, _) => "Unknown weather condition."
+    };
+}
+
+
+Console.WriteLine(WeatherCondition(30,"Sunny"));
+Console.WriteLine(WeatherCondition(1,"Sunny"));
+Console.WriteLine(WeatherCondition(1,"Rainy"));
+Console.WriteLine(WeatherCondition(25,"Rainy"));
+Console.WriteLine(WeatherCondition(40,"Rainy"));
+Console.WriteLine(WeatherCondition(12,"Cloudy"));
---
+++ 18-foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("FOREACH Examples");
+
+// in strings
+foreach(char c in "Hello")
+    Console.WriteLine(c);
+
+// in arrays
+var primes = new int[]{ 2, 3, 5, 7, 11, 13};
+foreach(int prime in primes)
+    Console.WriteLine(prime);
+
+// Lists
+var names = new List<string> { "Alice", "Bob", "Charlie" };
+foreach(var name in names)
+    Console.WriteLine(name);
+
+// and other "IEnumerable".. but that's another bedtime story
